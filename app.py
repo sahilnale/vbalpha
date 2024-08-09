cb26e5ecc56ac0cf72c0e1d9f52df3ed24fe70c9
Sahil <sahilnale@icloud.com>
2024-08-08 17:31:23 -0700

    deploy

--- app.py
+++ app.py
@@ -295,4 +295,5 @@ def add_signal():
     return render_template('add_signal.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
